f80501ac4f581fab2c9f77144d500842ffde9b7c
Lars Briem <briem.lars@googlemail.com>
2015-06-09 20:40:42 +0200

    create working transit schedule

--- transit/src/main/java/de/briemla/matsim/transit/TransitGenerator.java
+++ transit/src/main/java/de/briemla/matsim/transit/TransitGenerator.java
@@ -2,26 +2,34 @@ package de.briemla.matsim.transit;
 
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
-import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
-import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.Population;
-import org.matsim.api.core.v01.population.PopulationFactory;
-import org.matsim.api.core.v01.population.PopulationWriter;
-import org.matsim.core.api.internal.MatsimWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.population.routes.RouteUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.pt.transitSchedule.api.Departure;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitRouteStop;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt.transitSchedule.api.TransitScheduleFactory;
+import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
+import org.matsim.pt.transitSchedule.api.TransitStopFacility;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.Vehicles;
 
 /**
  * Create a {@link Population} where a {@link Person} is added at each
@@ -33,7 +41,7 @@ import org.matsim.core.utils.geometry.transformations.TransformationFactory;
  */
 public class TransitGenerator {
 	private static final String CONFIG_FILE = "./input/config.xml";
-	private static final String POPULATION_FILE = "./input/population.xml";
+	private static final String TRANSIT_SCHEDULE_FILE = "./input/transitschedule.xml";
 	private static final Duration MORNING_LEAVE_TIME = Duration.ofHours(6);
 	private static final Duration WORK_LEAVE_TIME = Duration.ofHours(16);
 
@@ -48,117 +56,71 @@ public class TransitGenerator {
 	private final Config config;
 	private final Scenario scenario;
 	private final Network network;
-	private final Population population;
-	private final PopulationFactory populationFactory;
-	private final boolean clearPopulation = true;
+	private final TransitSchedule transitSchedule;
+	private final TransitScheduleFactory transitScheduleFactory;
+	private final Vehicles transitVehicles;
 
 	public TransitGenerator() {
 		config = ConfigUtils.loadConfig(CONFIG_FILE);
 		scenario = ScenarioUtils.loadScenario(config);
 		network = scenario.getNetwork();
-		population = scenario.getPopulation();
-		if (clearPopulation) {
-			population.getPersons().clear();
-		}
-		populationFactory = population.getFactory();
+		transitSchedule = scenario.getTransitSchedule();
+		transitScheduleFactory = transitSchedule.getFactory();
+		transitVehicles = scenario.getTransitVehicles();
 	}
 
 	private void createSetup() {
-		createPopulation();
+		createTransitSchedule();
 	}
 
-	/**
-	 * Create a person at each {@link Node}
-	 */
-	private void createPopulation() {
-		network.getNodes().values().stream().forEach(this::createPerson);
-		savePopulation();
+	private void createTransitSchedule() {
+		// network.getNodes().values().stream().forEach(this::createPerson);
+		TransitStopFacility startStop = addStopFacility("1");
+		TransitStopFacility endStop = addStopFacility("3");
+
+		List<Id<Link>> routeIds = Arrays.asList(Id.createLinkId("11"), Id.createLinkId("12"), Id.createLinkId("23"),
+				Id.createLinkId("33"));
+		TransitRouteStop start = transitScheduleFactory.createTransitRouteStop(startStop, 0, 0);
+		start.setAwaitDepartureTime(true);
+		TransitRouteStop end = transitScheduleFactory.createTransitRouteStop(endStop, 9 * 60, 0);
+		end.setAwaitDepartureTime(true);
+
+		TransitLine transitLine = transitScheduleFactory.createTransitLine(Id.create("Tram 1", TransitLine.class));
+		TransitRoute transitRoute = createRoute(routeIds, start, end);
+		transitLine.addRoute(transitRoute);
+		transitSchedule.addTransitLine(transitLine);
+
+		saveTransitSchedule();
 	}
 
-	private void savePopulation() {
-		MatsimWriter popWriter = new PopulationWriter(population, network);
-		popWriter.write(POPULATION_FILE);
+	private TransitRoute createRoute(List<Id<Link>> routeIds, TransitRouteStop start, TransitRouteStop end) {
+		NetworkRoute route = RouteUtils.createNetworkRoute(routeIds, network);
+		List<TransitRouteStop> stops = Arrays.asList(start, end);
+		TransitRoute transitRoute = transitScheduleFactory.createTransitRoute(Id.create("2", TransitRoute.class),
+				route, stops, "pt");
+
+		Departure firstDeparture = transitScheduleFactory.createDeparture(Id.create(1, Departure.class), 6 * 60 * 60);
+		firstDeparture.setVehicleId(Id.create("tr_1", Vehicle.class));
+		transitRoute.addDeparture(firstDeparture);
+		return transitRoute;
 	}
 
-	/**
-	 * Create a new {@link Person} if there has no {@link Person} been created
-	 * for the current {@link Node}
-	 *
-	 * @param node
-	 *            to derive {@link Person} from
-	 */
-	private void createPerson(Node node) {
-		if (population.getPersons().containsKey(idFrom(node))) {
-			return;
+	private TransitStopFacility addStopFacility(String id) {
+		Node startNode = network.getNodes().get(Id.createNodeId(id));
+		Id<TransitStopFacility> stopId = Id.create(id, TransitStopFacility.class);
+		if (transitSchedule.getFacilities().containsKey(stopId)) {
+			return transitSchedule.getFacilities().get(stopId);
 		}
-		Person person = populationFactory.createPerson(idFrom(node));
-		population.addPerson(person);
-
-		Plan plan = createPlanFrom(node);
-		person.addPlan(plan);
+		TransitStopFacility startStop = transitScheduleFactory.createTransitStopFacility(stopId, startNode.getCoord(),
+				false);
+		startStop.setLinkId(Id.createLinkId(id + id));
+		transitSchedule.addStopFacility(startStop);
+		return startStop;
 	}
 
-	/**
-	 * Create a plan for the person. Start at node coordinates and travel to
-	 * center of map. Assuming that node coordinates are already in correct
-	 * coordinate system
-	 *
-	 * @param node
-	 *            start node for plan
-	 * @return new plan which starts at node, travels to center of map and
-	 *         travels back to node.
-	 */
-	private Plan createPlanFrom(Node node) {
-		Plan plan = populationFactory.createPlan();
-		Activity homeMorning = populationFactory.createActivityFromCoord("home", node.getCoord());
-		homeMorning.setEndTime(morningLeaveTime());
-		plan.addActivity(homeMorning);
-		plan.addLeg(populationFactory.createLeg("car"));
-		Activity workActivity = populationFactory.createActivityFromCoord("work", workCoordinate());
-		workActivity.setEndTime(workLeaveTime());
-		plan.addActivity(workActivity);
-		plan.addLeg(populationFactory.createLeg("car"));
-		Activity homeEvening = populationFactory.createActivityFromCoord("home", node.getCoord());
-		plan.addActivity(homeEvening);
-		return plan;
-	}
-
-	private Coord workCoordinate() {
-		ArrayList<? extends Node> nodes = new ArrayList<>(network.getNodes().values());
-		int nodeIndex = (int) (Math.random() * nodes.size());
-		return nodes.get(nodeIndex).getCoord();
-	}
-
-	private double workLeaveTime() {
-		return randomize(WORK_LEAVE_TIME).getSeconds();
-	}
-
-	private double morningLeaveTime() {
-		return randomize(MORNING_LEAVE_TIME).getSeconds();
-	}
-
-	/**
-	 * Randomly add a time between -60 and + 60 minutes. The method will return
-	 * a new {@link Duration} instance.
-	 *
-	 * @param time
-	 *            base {@link Duration} to add minutes to
-	 * @return new instance of {@link Duration} with added minutes
-	 */
-	private static Duration randomize(Duration time) {
-		long minutes = (long) ((Math.random() * 120) - 60);
-		return time.plusMinutes(minutes);
-	}
-
-	/**
-	 * Convert {@link Node} id to {@link Person} id.
-	 *
-	 * @param node
-	 *            to take id from
-	 * @return new id for a {@link Person}
-	 */
-	private static Id<Person> idFrom(Node node) {
-		return Id.createPersonId(node.getId().toString());
+	private void saveTransitSchedule() {
+		TransitScheduleWriter popWriter = new TransitScheduleWriter(transitSchedule);
+		popWriter.writeFile(TRANSIT_SCHEDULE_FILE);
 	}
 
 	private void startSimulation() {
@@ -170,7 +132,7 @@ public class TransitGenerator {
 		LocalTime start = LocalTime.now();
 
 		TransitGenerator generator = new TransitGenerator();
-		// generator.createSetup();
+		generator.createSetup();
 		LocalTime afterSetup = LocalTime.now();
 
 		generator.startSimulation();
